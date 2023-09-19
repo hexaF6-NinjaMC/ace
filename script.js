3563fd58ce0bf60eebb6f40a627380b4972ae8c4
hexaF6-NinjaMC <103971610+hexaF6-NinjaMC@users.noreply.github.com>
2023-09-19 15:04:43 -0500

    Create script.js
    added funcs and such

---
+++ script.js
@@ -0,0 +1,20 @@
+const htmlCode = document.querySelector('.html-code textarea');
+const cssCode = document.querySelector('.css-code textarea');
+const jsCode = document.querySelector('.js-code textarea');
+const result = document.querySelector('#result');
+
+function run() {
+  localStorage.setItem('html_code', htmlCode.value);
+  localStorage.setItem('css_code', cssCode.value);
+  localStorage.setItem('js_code', jsCode.value);
+  result.contentDocument.setHTML(`<style>${localStorage.css_code}</style>` + localStorage.html_code);
+  result.contentWindow.eval(localStorage.js_code);
+}
+
+htmlCode.onkeyup = () => run();
+cssCode.onkeyup = () => run();
+jsCode.onkeyup = () => run();
+
+htmlCode.value = localStorage.html_code;
+cssCode.value = localStorage.css_code;
+jsCode.value = localStorage.js_code;
