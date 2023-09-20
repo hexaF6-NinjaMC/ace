b08469baf7f15709fef0105101414c60247b0ec3
hexaF6-NinjaMC <bechtel.aaron22@gmail.com>
2023-09-19 21:55:11 -0500

    final initial changes, awaiting backend technology

--- script.js
+++ script.js
@@ -9,13 +9,32 @@ function run() {
   localStorage.setItem('js_code', jsCode.value);
   result.contentDocument.body.setHTML(`<style>${localStorage.css_code}</style>` + localStorage.html_code);
   result.contentWindow.eval(localStorage.js_code);
-  console.warn = () => {};
 }
 
 htmlCode.onkeyup = () => run();
 cssCode.onkeyup = () => run();
 jsCode.onkeyup = () => run();
 
-htmlCode.value = localStorage.html_code;
-cssCode.value = localStorage.css_code;
-jsCode.value = localStorage.js_code;
+if (localStorage.html_code == undefined) {
+  htmlCode.value = '';
+} else {
+  htmlCode.value = localStorage.html_code;
+}
+
+if (localStorage.css_code == undefined) {
+  cssCode.value = '';
+} else {
+  cssCode.value = localStorage.css_code;
+}
+
+if (localStorage.js_code == undefined) {
+  jsCode.value = '';
+} else {
+  jsCode.value = localStorage.js_code;
+}
+
+
+window.onload = () => {
+  result.contentDocument.body.setHTML(`<style>${cssCode.value}</style>` + htmlCode.value);
+  result.contentWindow.eval(jsCode.value);
+};
